7b69325197eb13af972f62aa68fcad08c6079af3
sumaya-kabir <sumayakabir4547@gmail.com>
2022-11-26 09:06:54 +0600

    categorywise product api

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 // const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -8,6 +9,37 @@ const app = express();
 // middleWare
 app.use(cors());
 app.use(express.json());
+
+// mongodb
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.if9xwsm.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        //collections
+        const carsCollection = client.db('heroCarsData').collection('cars');
+        
+
+        // APIs
+        app.get('/cars', async(req, res) => {
+            const query = {};
+            const cars = await carsCollection.find(query).toArray();
+            res.send(cars);
+        });
+
+        app.get('/cars/:category', async(req, res) => {
+            const category = req.params.category;
+            const query = {category: category};
+            const perCategory = await carsCollection.find(query).toArray();
+            res.send(perCategory);
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(console.log)
 
 app.get('/', async (req, res) => {
     res.send('Hero cars is running')
